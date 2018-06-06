8573af33c5885203abdfca58274d905c71d68fee
TheCB4 <cavelle@thecb4.io>
2018-06-05 21:15:27 -0400

    added determinePath for template download

--- Package.swift
+++ Package.swift
@@ -16,11 +16,12 @@ let package = Package(
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.1"),
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "4.2.0"),
         .package(url: "https://github.com/kylef/Spectre", from: "0.8.0"),
+        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0")
     ],
     targets: [
         .target(name: "Genesis", dependencies: ["GenesisCLI"]),
         .target(name: "GenesisCLI", dependencies: ["GenesisKit", "SwiftCLI"]),
-        .target(name: "GenesisKit", dependencies: ["SwiftCLI", "StencilSwiftKit", "Yams", "PathKit", "Rainbow"]),
+        .target(name: "GenesisKit", dependencies: ["SwiftCLI", "StencilSwiftKit", "Yams", "PathKit", "Rainbow", "ShellOut"]),
         .testTarget(name: "GenesisCLITests", dependencies: ["GenesisCLI", "Spectre"]),
         .testTarget(name: "GenesisKitTests", dependencies: ["GenesisKit", "Spectre"]),
     ]
