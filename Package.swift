bfa599f1afc4ee5d5cbf63395f3917461b2d5da1
Yonas Kolb <yonaskolb@gmail.com>
2018-11-08 18:34:22 +1100

    remove spectre

--- Package.swift
+++ Package.swift
@@ -15,13 +15,12 @@ let package = Package(
         .package(url: "https://github.com/kylef/PathKit", from: "0.9.0"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "3.1.0"),
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.2.0"),
-        .package(url: "https://github.com/kylef/Spectre", from: "0.9.0"),
     ],
     targets: [
         .target(name: "Genesis", dependencies: ["GenesisCLI"]),
         .target(name: "GenesisCLI", dependencies: ["GenesisKit", "SwiftCLI"]),
         .target(name: "GenesisKit", dependencies: ["SwiftCLI", "StencilSwiftKit", "Yams", "PathKit", "Rainbow"]),
-        .testTarget(name: "GenesisCLITests", dependencies: ["GenesisCLI", "Spectre"]),
-        .testTarget(name: "GenesisKitTests", dependencies: ["GenesisKit", "Spectre"]),
+        .testTarget(name: "GenesisCLITests", dependencies: ["GenesisCLI"]),
+        .testTarget(name: "GenesisKitTests", dependencies: ["GenesisKit"]),
     ]
 )
