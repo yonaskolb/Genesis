d08d3e445bc6f56e2776946ce20c297e92379b30
guseducampos <guseducampos@gmail.com>
2019-10-14 17:58:42 -0600

    fix CLI tests

--- Tests/GenesisCLITests/CLITests.swift
+++ Tests/GenesisCLITests/CLITests.swift
@@ -6,8 +6,8 @@ public class CLITests: XCTestCase {
 
     func testCLI() {
         let cli = GenesisCLI()
-        XCTAssertEqual(cli.run(arguments: "genesis"), 1)
-        XCTAssertEqual(cli.run(arguments: "genesis invalid"), 1)
-        XCTAssertEqual(cli.run(arguments: "genesis -h"), 0)
+        XCTAssertEqual(cli.run(arguments: ["genesis"]), 1)
+        XCTAssertEqual(cli.run(arguments: ["genesis", "invalid"]), 1)
+        XCTAssertEqual(cli.run(arguments: ["generate", "-h"]), 0)
     }
 }
